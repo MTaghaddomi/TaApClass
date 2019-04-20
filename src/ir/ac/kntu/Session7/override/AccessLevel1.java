a2b22eda76b05dc2c6a6f74eb4cfa6284f4c6dc5
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-04-20 17:38:28 +0430

    doc added

--- src/ir/ac/kntu/Session7/override/AccessLevel1.java
+++ src/ir/ac/kntu/Session7/override/AccessLevel1.java
@@ -1,10 +1,12 @@
 package ir.ac.kntu.Session7.override;
 
+
 public class AccessLevel1 {
 
 }
 
-
+// for more details you can see this link: 
+// http://www.xyzws.com/javafaq/can-an-overriding-method-have-a-different-return-type-than-the-overridden-method/39
 class A {
     void m() {
         System.out.println("m super");
@@ -29,6 +31,7 @@ class B extends A {
 
     //access level of the overridden method (subclass method),
     // should be stronger(higher) than super class
+    @Override
     public void m() {
         System.out.println("m sub");
     }
@@ -36,15 +39,16 @@ class B extends A {
     //overridden
     @Override
     public String m2(String s) {
-        System.out.println(s + " in sub");
+        System.out.println("m2 sub");
         return s;
     }
 
     
     //NOT overridden!!
     //@Override cause compile error
+    // overloaded method
     public void m3(Object s) {
-        System.out.println("m3 super");
+        System.out.println("m3 sub");
     }
 
     //NOT overridden!!
